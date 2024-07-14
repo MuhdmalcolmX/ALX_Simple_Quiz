ad9896e705b23c1119e37101598078ccec654136
Mohammed Sylla <muhdmalcolmx@gmail.com>
2024-07-14 22:48:49 +0000

    update quiz.js file

--- quiz.js
+++ quiz.js
@@ -1,32 +1,64 @@
 function checkAnswer() {
     // Function body
+
+    // Identify the Correct Answer
     let correctAnswer = 4;
 
-    var userAnswer = document.querySelector('input[name="quiz"]:checked');
+    // Retrieve the User’s Answer
+    var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    console.log(userAnswer);
 
-    if (userAnswer == correctAnswer) {
-        const changeText = document.querySelector("feedback");
-        changeText.textContent = "Correct! Well done.";
-    }
-    else {
-        const changeText = document.querySelector("feedback");
-        changeText.textContent = "That's incorrect. Try again!";
+    // Compare the User’s Answer with the Correct Answer
+    if (userAnswer == 4) {
+        const para = document.getElementById('feedback').textContent = "Correct! Well done.";
+        console.log(para);     
+    } else {
+        const para = document.getElementById('feedback').textContent = "That's incorrect. Try again!";
+        console.log(para);  
     }
 
-    document.getElementById("submit-answer").addEventListener('click', function() {
-        changeText;
+    // Add an Event Listener to the Submit Butto
+    document.getElementById("submit-answer").addEventListener('click', function checkAnswer() {
+        
     })
 
-
-    // if(userAnswer != null) {
-    //     document.getElementById("disp").innerHTML
-    //         = userAnswer.value
-    //         + " radio button checked";
-    // }
-    // else {
-    //     document.getElementById("disp").innerHTML
-    //         = "No one selected";
-    // } 
-
-
 }
+
+    
+
+    // document.getElementById("submit-answer").addEventListener('click', function() {
+    //     var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    //     console.log(userAnswer);
+
+    //     if (userAnswer == 4) {
+    //         const para = document.getElementById('feedback').textContent = "Correct! Well done.";
+    //         console.log(para);     
+    //     } else {
+    //         const para = document.getElementById('feedback').textContent = "That's incorrect. Try again!";
+    //         console.log(para);  
+    //     }
+
+    // }) 
+        
+
+    //     if (userAnswer == 4) {
+    //         const changeText = document.querySelector("feedback").textContent = "Correct";
+    //         // changeText.textContent = "";
+    //     }
+    //     else {
+    //         const changeText = document.querySelector("feedback");
+    //         // changeText.textContent = "That's incorrect. Try again!";
+    //     }
+    // })
+
+
+    // var userAnswer = document.querySelector('input[name="quiz"]:checked');
+    
+    // if (userAnswer == 4) {
+    //     const tryit = document.getElementById("feedback").textContent = "Correct! Well done.";
+    //     console.log(tryit);
+    // }
+    
+
+    
+
