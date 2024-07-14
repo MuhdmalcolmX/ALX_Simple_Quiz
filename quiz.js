b9a9489a861dba5b6da4d24cdb2acf88bb4fe798
Mohammed Sylla <muhdmalcolmx@gmail.com>
2024-07-14 22:49:46 +0000

    update quiz.js file again

--- quiz.js
+++ quiz.js
@@ -9,7 +9,7 @@ function checkAnswer() {
     console.log(userAnswer);
 
     // Compare the User’s Answer with the Correct Answer
-    if (userAnswer == 4) {
+    if (userAnswer == correctAnswer) {
         const para = document.getElementById('feedback').textContent = "Correct! Well done.";
         console.log(para);     
     } else {
