0a8197587739e3a108dfbc73da8b62e64d400bad
Mohammed Sylla <muhdmalcolmx@gmail.com>
2024-07-14 16:27:42 +0000

    update

--- quiz.js
+++ quiz.js
@@ -1,3 +1,32 @@
 function checkAnswer() {
     // Function body
+    let correctAnswer = 4;
+
+    var userAnswer = document.querySelector('input[name="quiz"]:checked');
+
+    if (userAnswer == correctAnswer) {
+        const changeText = document.querySelector("feedback");
+        changeText.textContent = "Correct! Well done.";
+    }
+    else {
+        const changeText = document.querySelector("feedback");
+        changeText.textContent = "That's incorrect. Try again!";
+    }
+
+    document.getElementById("submit-answer").addEventListener('click', function() {
+        changeText;
+    })
+
+
+    // if(userAnswer != null) {
+    //     document.getElementById("disp").innerHTML
+    //         = userAnswer.value
+    //         + " radio button checked";
+    // }
+    // else {
+    //     document.getElementById("disp").innerHTML
+    //         = "No one selected";
+    // } 
+
+
 }
