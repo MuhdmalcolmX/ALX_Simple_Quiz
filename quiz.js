9ab19dd576a71a06b2cc1dfb729a02ee9e08ce7b
Mohammed Sylla <muhdmalcolmx@gmail.com>
2024-07-14 23:25:53 +0000

    Final working quiz.js file update

--- quiz.js
+++ quiz.js
@@ -5,16 +5,20 @@ function checkAnswer() {
     let correctAnswer = 4;
 
     // Retrieve the User’s Answer
-    var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
+    var userAnswer = parseFloat(document.querySelector('input[name="quiz"]:checked').value);
     //console.log(userAnswer);
 
     // Compare the User’s Answer with the Correct Answer
     if (userAnswer === correctAnswer) {
-        const para = document.getElementById('feedback').textContent = "Correct! Well done.";
-        //console.log(para);     
+        document.getElementById('feedback').textContent = "Correct! Well done.";
+
+        // const para = document.getElementById('feedback').textContent = "Correct! Well done.";
+        // console.log(para);     
     } else {
-        const para = document.getElementById('feedback').textContent = "That's incorrect. Try again!";
-        //console.log(para);  
+        document.getElementById('feedback').textContent = "That's incorrect. Try again!";
+
+        // const para = document.getElementById('feedback').textContent = "That's incorrect. Try again!";
+        // console.log(para);  
     }
 }
 
