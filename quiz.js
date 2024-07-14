905f1bbb6bd67cf073f23e11fe1ee6051199ca1b
Mohammed Sylla <muhdmalcolmx@gmail.com>
2024-07-14 23:19:28 +0000

    final update to quiz.js working file

--- quiz.js
+++ quiz.js
@@ -6,59 +6,20 @@ function checkAnswer() {
 
     // Retrieve the User’s Answer
     var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
-    console.log(userAnswer);
+    //console.log(userAnswer);
 
     // Compare the User’s Answer with the Correct Answer
-    if (userAnswer === correctAnswer) {
+    if (userAnswer == correctAnswer) {
         const para = document.getElementById('feedback').textContent = "Correct! Well done.";
-        console.log(para);     
+        //console.log(para);     
     } else {
         const para = document.getElementById('feedback').textContent = "That's incorrect. Try again!";
-        console.log(para);  
+        //console.log(para);  
     }
-
-    // Add an Event Listener to the Submit Butto
-    document.getElementById("submit-answer").addEventListener('click', function checkAnswer() {
-        
-    })
-
 }
 
-    
 
-    // document.getElementById("submit-answer").addEventListener('click', function() {
-    //     var userAnswer = document.querySelector('input[name="quiz"]:checked').value;
-    //     console.log(userAnswer);
-
-    //     if (userAnswer == 4) {
-    //         const para = document.getElementById('feedback').textContent = "Correct! Well done.";
-    //         console.log(para);     
-    //     } else {
-    //         const para = document.getElementById('feedback').textContent = "That's incorrect. Try again!";
-    //         console.log(para);  
-    //     }
-
-    // }) 
-        
-
-    //     if (userAnswer == 4) {
-    //         const changeText = document.querySelector("feedback").textContent = "Correct";
-    //         // changeText.textContent = "";
-    //     }
-    //     else {
-    //         const changeText = document.querySelector("feedback");
-    //         // changeText.textContent = "That's incorrect. Try again!";
-    //     }
-    // })
-
-
-    // var userAnswer = document.querySelector('input[name="quiz"]:checked');
-    
-    // if (userAnswer == 4) {
-    //     const tryit = document.getElementById("feedback").textContent = "Correct! Well done.";
-    //     console.log(tryit);
-    // }
-    
+document.getElementById("submit-answer").addEventListener('click', checkAnswer);
 
     
 
